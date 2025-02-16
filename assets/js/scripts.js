87ba466e84c10cc39c17dc4f2e8e09bbdb45ff09
ahmed_gebril <ahmed.m.gebriel@gmail.com>
2025-02-16 20:22:01 +0200

    --

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,7 +14,7 @@ $(document)
 
 //FUNCTIONS
 function onDocumentReady() {
-    if (location.href.includes('127.0.0.1')) {
+    if (location.href.includes('127.0.0.1') || location.href.includes('surge')) {
         $("header").load("partial/header.html");
         $("footer").load("partial/footer.html");
     }
